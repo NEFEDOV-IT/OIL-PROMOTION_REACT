0cb4a5b7eafa7972ab0ac0599a05778a4aba768c
NEFEDOV_IT <eliaz_one@mail.ru>
2022-08-05 22:56:58 +0300

    add build

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-    <BrowserRouter>
+    <BrowserRouter basename="/_sites/oil-promotion">
         <ThemeProvider theme={theme}>
             <App/>
         </ThemeProvider>
